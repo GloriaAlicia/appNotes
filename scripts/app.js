779d0abef6d98b3b8443771fe67831e045b46728
Gloria Alicia <ga365743@gmail.com>
2022-04-25 10:18:54 -0500

    error

--- scripts/app.js
+++ scripts/app.js
@@ -7,35 +7,29 @@ function openModal() {
 }
 
 let buttonNew = document.getElementById("new");
-buttonNew.addEventListener("click", newNote)
+buttonNew.addEventListener("click", newNote);
 
 function newNote() {
 /**no pude usar event.preventdefault() porque no use una arrow function, y no la use porque creo que podría usar la misma función en otro lado aunque no estoy segura */  
 /**agregar el contendor principal */
     let wrapper = document.querySelector(".wrapper")
     let note = document.createElement("article");
+    note.classList.add("notes");
+    wrapper.appendChild(note);
 /**crear titulo, dar contenido y agregar al subcontendor */
     let title = document.createElement("h3")
+    title.textContent = document.getElementById("titulo").value;
+    note.appendChild(title);
+
     let text = document.createElement("p")
+    text.textContent = document.getElementById("text").value;
+    text.classList.add("text");
+    note.appendChild(text);
 /**validar entrada */
-    let validarTitulo = validarEntrada(title);
-    let validarTexto = validarEntrada(text);
-    if(!validarTitulo){
-        console.log("Titulo Vacío")
-    } else{
-        title.textContent = document.getElementById("titulo").value;
-        note.appendChild(title);
-    }
-    if(!validarTexto){
-        console.log("Texto vacio")
-    } else{
-        text.textContent = document.getElementById("text").value;
-        text.classList.add("text");
-        note.appendChild(text);
-    }
-    if (validarTitulo && validarTexto) {
-        wrapper.appendChild(note);
-        note.classList.add("notes");
+    if(validarEntrada(text)){
+        alert("texto insuficiente");
+    } else {
+        alert("save")
     }
 
     modal.classList.add("hidden");
@@ -44,13 +38,14 @@ function newNote() {
     document.getElementById("text").value = "";
 }
 function validarEntrada(element){
-    if (element.lenght >= 1){
-        return true; 
-    }else{
-        return false;
-    }
+     if (element.length === 0){
+        console.log("Titulo Vacío")
+        return  true
+     }
+    return false
 }
-console.log(text.value)
+
+
 /**close button */
 let buttonClose = document.getElementById("close");
 buttonClose.addEventListener("click", closen)
