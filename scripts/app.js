37d609c851f07a7507de304a38fc51cb0f5f454d
Gloria Alicia <ga365743@gmail.com>
2022-04-27 17:51:07 -0500

    mostrar y gestionar errores de entrada listos

--- scripts/app.js
+++ scripts/app.js
@@ -7,63 +7,77 @@ function openModal() {
 }
 
 let buttonNew = document.getElementById("new");
-buttonNew.addEventListener("click", newNote);
+buttonNew.addEventListener("click", verificarYCrear);
 
-function newNote() {
-/**no pude usar event.preventdefault() porque no use una arrow function, y no la use porque creo que podría usar la misma función en otro lado aunque no estoy segura */  
-/**agregar el contendor principal */
-    let wrapper = document.querySelector(".wrapper")
+let fields = document.querySelector(".field")
+let errorTitulo = document.getElementById("errorTitulo");
+let errorTexto = document.getElementById("errorTexto");
+
+function validarEntrada(inputs){
+    if (inputs.length == 0){
+       return  true;
+    }
+   return false
+}
+
+function verificarYCrear() {
+    let entradaTitulo = document.getElementById("titulo").value;
+    let entradaTexto = document.getElementById("text").value;
+
     let note = document.createElement("article");
-    note.classList.add("notes");
-    wrapper.appendChild(note);
-/**crear titulo, dar contenido y agregar al subcontendor */
-    let title = document.createElement("h3")
-    title.textContent = document.getElementById("titulo").value;
-    note.appendChild(title);
-
-    let text = document.createElement("p")
-    text.textContent = document.getElementById("text").value;
-    text.classList.add("text");
-    note.appendChild(text);
-/**validar entrada */
-    if(validarEntrada(text)){
-        alert("texto insuficiente");
-    } else {
-        alert("save")
+    /**no pude usar event.preventdefault() porque no use una arrow function, y no la use porque creo que podría usar la misma función en otro lado aunque no estoy segura */ 
+    if(!validarEntrada(entradaTitulo)) {
+        errorTitulo.classList.add("hidden");
+        note.appendChild(crearElementos("h3", entradaTitulo));
+    }
+    if (!validarEntrada(entradaTexto)) {
+        errorTexto.classList.add("hidden");
+        note.appendChild(crearElementos("p",entradaTexto,"text"));
+    }
+    if(!validarEntrada(entradaTitulo) && !validarEntrada(entradaTexto)){
+        let wrapper = document.querySelector(".wrapper");
+        note.classList.add("notes");
+        wrapper.appendChild(note);
+        closen();
     }
 
-    modal.classList.add("hidden");
-    /**resetear el valor de los input */
-    document.getElementById("titulo").value = "";
-    document.getElementById("text").value = "";
-}
-function validarEntrada(element){
-     if (element.length === 0){
-        console.log("Titulo Vacío")
-        return  true
-     }
-    return false
+/**si la entrada esta vacía muestra error */
+    if(validarEntrada(entradaTitulo)){
+        errorTitulo.classList.remove("hidden");
+    }
+    if(validarEntrada(entradaTexto)){
+        errorTexto.classList.remove("hidden");
+    }
 }
 
+function crearElementos(element,dato,clase) {
+    let creando = document.createElement(element);
+    creando.classList.add(clase);
+    creando.textContent = dato;
+    return creando;
+}
 
 /**close button */
 let buttonClose = document.getElementById("close");
-buttonClose.addEventListener("click", closen)
-
+buttonClose.addEventListener("click", closen);
 function closen(){
-    modal.classList.add("hidden")
+    modal.classList.add("hidden");
+    document.getElementById("titulo").value = "";
+    document.getElementById("text").value = "";
+    errorTitulo.classList.add("hidden");
+    errorTexto.classList.add("hidden");
 }
 /**modo oscuro provicional  */
     let button = document.getElementById("activar");
 
 button.addEventListener("click", modo)
 function modo() {
-    let inputs = document.querySelector(".entradas");
-    let textarea = document.getElementById("text")
+    let titulo = document.getElementById("titulo");
+    let textarea = document.getElementById("text");
     let todo = document.getElementById("body");
     let modal = document.querySelector(".modal");
     modal.classList.toggle("oscuro");
     todo.classList.toggle("oscuro");
-    inputs.classList.toggle("oscuro");
+    titulo.classList.toggle("oscuro");
     textarea.classList.toggle("oscuro");
 }
